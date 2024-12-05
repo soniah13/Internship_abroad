3fa9ea9eb55e2bcff7624728050cbb2d5a106e73
imorgansonia <imorgansonia@gmail.com>
2024-12-05 13:31:24 +0300

    added lost inputs

--- Abroad/src/Components/JobPost.jsx
+++ Abroad/src/Components/JobPost.jsx
@@ -18,7 +18,7 @@ function JobPost({ countryName, countryId }) {
     application_deadline: '',
     applicant_count: 0,
     company_logo: null,
-    picture: null,
+    picture:'https://res.cloudinary.com/ddqkfdqy8/image/upload/v1730976302/qjliy417egm4jxavpanl.png',
     standard_image: 'https://res.cloudinary.com/ddqkfdqy8/image/upload/v1730976302/qjliy417egm4jxavpanl.png',
   });
 
@@ -99,7 +99,7 @@ function JobPost({ countryName, countryId }) {
             application_deadline: '',
             applicant_count: 0,
             company_logo: null,
-            picture: null,
+            picture: 'https://res.cloudinary.com/ddqkfdqy8/image/upload/v1730976302/qjliy417egm4jxavpanl.png',
             standard_image: 'https://res.cloudinary.com/ddqkfdqy8/image/upload/v1730976302/qjliy417egm4jxavpanl.png',
           });
           setErrors({});
@@ -194,8 +194,41 @@ function JobPost({ countryName, countryId }) {
       />
       {errors.job_description && <p className="text-red-500 mb-4">{errors.job_description}</p>}
 
-      {/* Other fields (About Company, Responsibilities, etc.) */}
-      {/* Similar to above inputs for other fields, handling errors accordingly */}
+      <textarea
+        name="about_company" placeholder="About the Company" value={formData.about_company} onChange={handleChange}
+        className="w-full p-3 mb-1 border border-gray-300 rounded focus:outline-none focus:border-blue-500" required
+      />
+      {errors.about_company && <p className="text-red-500 mb-4">{errors.about_company}</p>}
+      
+      <textarea
+        name="qualifications" placeholder="Job qualifications" value={formData.qualifications} onChange={handleChange}
+        className="w-full p-3 mb-1 border border-gray-300 rounded focus:outline-none focus:border-blue-500" required
+      />
+      {errors.qualifications && <p className="text-red-500 mb-4">{errors.qualifications}</p>}
+
+      <textarea
+        name="benefits" placeholder="what does the student gain?" value={formData.benefits} onChange={handleChange}
+        className="w-full p-3 mb-1 border border-gray-300 rounded focus:outline-none focus:border-blue-500" required
+      />
+      {errors.benefits && <p className="text-red-500 mb-4">{errors.benefits}</p>}
+
+      <textarea
+        name="responsibilities" placeholder="Will be responsible for?" value={formData.responsibilities} onChange={handleChange}
+        className="w-full p-3 mb-1 border border-gray-300 rounded focus:outline-none focus:border-blue-500" required
+      />
+      {errors.responsibilities && <p className="text-red-500 mb-4">{errors.responsibilities}</p>}
+
+      <input
+        type="date" name="application_deadline" value={formData.application_deadline} onChange={handleChange}
+        className="w-full p-3 mb-1 border border-gray-300 rounded focus:outline-none focus:border-blue-500" required
+      />
+      {errors.application_deadline && <p className="text-red-500 mb-4">{errors.application_deadline}</p>}
+
+      <input
+        type="file" name="company_logo" onChange={handleFileChange}
+        className="w-full p-3 mb-1 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+      />
+      {errors.company_logo && <p className="text-red-500 mb-4">{errors.company_logo}</p>}
 
       <button type="submit" className="bg-blue-600 hover:bg-blue-300 text-white font-semibold py-2 px-8 rounded items-center justify-center w-full text-2xl mt-4">
         Submit Job
